294115b3ed25f4543d2e9c856d18ddfa8e594b2b
Maud <18214541+maud-lv@users.noreply.github.com>
2024-01-17 14:44:09 +0100

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace TestAppConfig.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public Settings Settings { get; }
+
+    public IndexModel(IOptionsSnapshot<Settings> options, ILogger<IndexModel> logger)
+    {
+        Settings = options.Value;
+        _logger = logger;
+    }
+}
